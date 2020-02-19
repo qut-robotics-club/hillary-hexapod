3da69308d87aeaec3158d2e32bd35470367e7b89
CallumJHays <callumjhays@gmail.com>
2020-02-19 17:54:48 +1000

    remove unnecessary

--- next.config.js
+++ next.config.js
@@ -2,14 +2,8 @@ const withCSS = require("@zeit/next-css");
 const withOffline = require("next-offline");
 const { ProvidePlugin } = require("webpack");
 
-const { NODE_ENV } = process.env;
-
 module.exports = withCSS(
   withOffline({
-    env: {
-      DEV_MODE: NODE_ENV === "development"
-    },
-
     webpack: config => {
       config.plugins.push(
         new ProvidePlugin({
